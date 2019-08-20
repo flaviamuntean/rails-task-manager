7c4df3af998ebd752dff4cc62441e805877328db
Flavia Muntean <muntean.flavia@gmail.com>
2019-08-20 13:13:12 +0100

    tasks 1-5 done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # show all tasks
+  get 'tasks', to: "tasks#index"
+
+  # add new task
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  # show one task
+  get 'tasks/:id', to: "tasks#show", as: :task
 end
